5f2a605cc40f1a81ffe83bde5ba1f70be9ba3963
Nathan Fraysse <foxou06@gmail.com>
2026-02-18 21:26:52 +0100

    dede

--- js/valo.js
+++ js/valo.js
@@ -1,3 +1,19 @@
 document.getElementById('scrollTopBtn').addEventListener('click', () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 });
+
+const burgerBtn = document.getElementById('burgerBtn');
+const nav = document.querySelector('nav');
+const navLinks = document.querySelectorAll('.nav-links a');
+
+burgerBtn.addEventListener('click', () => {
+    burgerBtn.classList.toggle('open');
+    nav.classList.toggle('open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        burgerBtn.classList.remove('open');
+        nav.classList.remove('open');
+    });
+});
